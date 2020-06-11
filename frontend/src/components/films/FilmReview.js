16988a90a8aad6d864f362e5207c8fd2e935a1a5
Gabriel Goiri <gabrielgoiri@gmail.com>
2020-06-11 10:14:03 +0100

    styling the reviews container

--- frontend/src/components/films/FilmReview.js
+++ frontend/src/components/films/FilmReview.js
@@ -4,8 +4,8 @@ const FilmReview = ({ owner, text, rating }) => (
   <div className="review">
     <div className="review-container">
       <div className="review-header">
-        <span>{rating}</span>
-        <h4>{owner.username}</h4>
+        <span>Rating: {rating}/5</span>
+        <h4>User: {owner.username}</h4>
       </div>
       <div className="review-content">
         <p> {text} </p>
